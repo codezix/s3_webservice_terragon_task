747940c27e96a82deb535e9bfd8980a36ea554de
chimdi <chimdiepignosis@gmail.com>
2019-12-13 23:02:41 +0100

    amazon s3 webservice upload file

---
+++ create-bucket.js
@@ -0,0 +1,28 @@
+const AWS = require('aws-sdk');
+
+
+const ID = '';
+const SECRET = '';
+
+
+const BUCKET_NAME = 'sga6-test';
+
+
+
+const s3 = new AWS.S3({
+    accessKeyId: ID,
+    secretAccessKey: SECRET
+});
+
+const params = {
+    Bucket: BUCKET_NAME,
+    CreateBucketConfiguration: {
+        // Set your region here
+        LocationConstraint: "eu-west-1"
+    }
+};
+
+s3.createBucket(params, function(err, data) {
+    if (err) console.log(err, err.stack);
+    else console.log('Bucket Created Successfully', data.Location);
+});
